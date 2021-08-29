dd1387d5fe496bb4b04f7221da137abbd27dbc17
dr666m1 <skndr666m1@gmail.com>
2021-08-29 12:11:30 +0900

    Add test cases

--- tests/test_npspy.py
+++ tests/test_npspy.py
@@ -1,5 +1,47 @@
-from npspy import __version__
+import pytest
+from npspy import InvalidAnswerError, categorize, calculate
+
+def test_categorize_negative():
+    with pytest.raises(InvalidAnswerError):
+        categorize(-1)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_categorize_0():
+    assert categorize(0) == "detractor"
+
+
+def test_categorize_6():
+    assert categorize(6) == "detractor"
+
+
+def test_categorize_7():
+    assert categorize(7) == "passive"
+
+
+def test_categorize_8():
+    assert categorize(8) == "passive"
+
+
+def test_categorize_9():
+    assert categorize(9) == "promoter"
+
+
+def test_categorize_10():
+    assert categorize(10) == "promoter"
+
+
+def test_categorize_more_than_10():
+    with pytest.raises(InvalidAnswerError):
+        assert categorize(11)
+
+
+def test_calculate_0():
+    assert calculate([0, 9]) == 0
+
+
+def test_calculate_33():
+    assert calculate([7, 8, 9]) == 1/3 * 100
+
+
+def test_calculate_100():
+    assert calculate([9, 10]) == 100
